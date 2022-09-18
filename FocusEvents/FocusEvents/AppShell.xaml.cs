e406f0ea2535eaca1161ae80da004a6f0dfc5025
Karthik Raja AKR <41435300+KarthikRajaAKR@users.noreply.github.com>
2022-09-18 20:34:54 +0530

    Add files via upload

---
+++ FocusEvents/FocusEvents/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FocusEvents;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ FocusEvents/FocusEvents/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿namespace FocusEvents;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void Button_Focused(object sender, FocusEventArgs e)
+	{
+
+    }
+
+	private void Button_Unfocused(object sender, FocusEventArgs e)
+	{
+
+    }
+
+	private void OnTapGestureRecognizerTapped(object sender, EventArgs e)
+	{
+
+    }
+
+	private void OnLayoutOneFocused(object sender, FocusEventArgs e)
+	{
+
+    }
+
+	private void OnLayoutOneUnfocused(object sender, FocusEventArgs e)
+	{
+
+    }
+
+    private void OnLayoutTwoFocused(object sender, FocusEventArgs e)
+    {
+
+    }
+
+    private void OnLayoutTwoUnfocused(object sender, FocusEventArgs e)
+    {
+
+    }
+}
+
