ef6ce3ad9ad4cbcb9ef311ab706ee9ba4df817ce
Karthik Raja AKR <41435300+KarthikRajaAKR@users.noreply.github.com>
2023-06-27 13:01:27 +0530

    Add files via upload

---
+++ ImageBindingSample/ImageBindingSample/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace ImageBindingSample;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+  
+}
+
+internal class ImageViewModel
+{
+    public ImageSource ImageSource { get; set; }
+
+    public ImageViewModel()
+    {
+        var byteArray = File.ReadAllBytes(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "dotnet_bot.png"));
+        this.ImageSource = ImageSource.FromStream(() => new MemoryStream(byteArray));
+    }
+}
